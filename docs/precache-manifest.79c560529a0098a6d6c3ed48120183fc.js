c3ad3b5bc780db5fe0992b33c507790c7c0f2b9a
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-09-30 11:09:36 +0200

    add docs folder

---
+++ docs/precache-manifest.79c560529a0098a6d6c3ed48120183fc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f4fe7a1fa1efbcc0c00f313b32563e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51e1ed78965bea2822d3",
+    "url": "./static/css/main.b6122036.chunk.css"
+  },
+  {
+    "revision": "be9f136565835a5a2046",
+    "url": "./static/js/2.88f8b16f.chunk.js"
+  },
+  {
+    "revision": "37264d371824fe0d96b5fb4e64891a84",
+    "url": "./static/js/2.88f8b16f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e1ed78965bea2822d3",
+    "url": "./static/js/main.262b5de9.chunk.js"
+  },
+  {
+    "revision": "4c3c83e9b568ed6309a7",
+    "url": "./static/js/runtime-main.8a8bec13.js"
+  },
+  {
+    "revision": "d921ad3a99c526533f879400bbcbc4b1",
+    "url": "./static/media/CAP1_1.d921ad3a.jpg"
+  },
+  {
+    "revision": "12b1f4b481e0204668aec82d4ec3f685",
+    "url": "./static/media/CAP1_2.12b1f4b4.jpg"
+  },
+  {
+    "revision": "3af4228799d79f2f0bf700216e9a1cab",
+    "url": "./static/media/CAP1_3.3af42287.jpg"
+  },
+  {
+    "revision": "3aa36ff3e2ca76c6d1001ae19c5dea64",
+    "url": "./static/media/CAP1_4.3aa36ff3.jpg"
+  },
+  {
+    "revision": "60723a372f1e6cdef64ce6683b53331a",
+    "url": "./static/media/CAP1_5.60723a37.jpg"
+  },
+  {
+    "revision": "e20729b7d7482f3d0dd860cfa8df6a67",
+    "url": "./static/media/CAP1_6.e20729b7.jpg"
+  },
+  {
+    "revision": "1404f99f45157606a1f6839e88b46335",
+    "url": "./static/media/home.1404f99f.jpg"
+  }
+]);
